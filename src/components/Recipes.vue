a2f31303c28cf7da622121c41edd29dfec6a1cb9
gregoryshoniwa <gregoryshoniwa@gmail.com>
2022-03-16 00:09:12 +0200

    search a single recipe

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -3,12 +3,16 @@
     <v-col>
       <v-card max-width="900" class="mx-auto">
         <v-toolbar color="blue" dark>
-          <v-toolbar-title v-if="recipes.length > 0">Recipe List</v-toolbar-title>
+          <v-toolbar-title v-if="recipes.length > 0"
+            >Recipe List</v-toolbar-title
+          >
 
           <v-spacer></v-spacer>
 
           <v-text-field
             v-if="recipes.length > 0"
+            v-model="searchText"
+            v-on:keyup.enter="onEnter"
             class="mt-8"
             outlined
             dense
@@ -19,7 +23,11 @@
         <div class="recipes">
           <v-list dense>
             <v-list-item-group color="primary">
-              <v-list-item v-for="(item, i) in recipes" :key="i" @click="getRecipeIndex(i)">
+              <v-list-item
+                v-for="(item, i) in recipes"
+                :key="i"
+                @click="getRecipeIndex(i)"
+              >
                 <v-list-item-icon>
                   <v-icon>mdi-silverware-fork-knife</v-icon>
                 </v-list-item-icon>
@@ -38,21 +46,39 @@
 <script>
 export default {
   data: () => ({
-    
+    results: [],
+    searchText: "",
+    status: false,
   }),
   computed: {
     recipes() {
       return this.$store.getters.getRecipes;
     },
+    filterData(){
+      return this.searchText.split(",");
+    }
   },
-  
+  watch: {},
   methods: {
-      getRecipeIndex(index){
-        
-          this.$store.dispatch("setRecipeIndex", index);
-          
-      }
-  }
+   
+   onEnter() {
+    //  var results = this.recipes.filter((val) =>{
+    //    return this.filterData.find((val2) =>{
+    //      return val.recipe.includes(val2)
+    //    })
+    //  })
+    console.log(this.filterData)
+    const res = this.recipes.filter(item => item.ingredients.some(
+        ingredient => this.filterData.every(f => ingredient.includes(f)))
+      )
+    
+
+   console.log(res)
+    },
+    getRecipeIndex(index) {
+      this.$store.dispatch("setRecipeIndex", index);
+    },
+  },
 };
 </script>
 
